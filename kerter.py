8598b0a3288203179e51e11daa04f70f6a4acf61
Herman <otvosmate519@gmail.com>
2020-10-06 19:02:57 +0200

    zzz

--- kerter.py
+++ kerter.py
@@ -3,4 +3,11 @@ def teglalapKerulet():
 	a=input("Kérem a téglalap egyik oldalát[cm]:")	
 	b=input("Kérem a téglalap másik oldalát[cm]:")
 	return float(2*(a+b))
+
+def nyolcszogKerulet():
+	a = input("Kérem adja meg a nyolcszög egyik oldalát[cm]")
+	b = input("Kérem adja meg a nyolcszög második oldalát[cm]")
+	c = input("Kérem adja meg a nyolcszög harmadik oldalát[cm]")
+	d = input("Kérem adja meg a nyolcszög negyedik oldalát[cm]")
 	
+	return float(2*(a+b+c+d)

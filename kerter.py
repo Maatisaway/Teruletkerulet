93c6fc5028ff34232613ae987366ee8c1b965510
Szoftver Szilard <szoftteszt2020@gmail.com>
2020-10-06 18:57:34 +0200

    Teglalap Terulet

--- kerter.py
+++ kerter.py
@@ -3,4 +3,8 @@ def teglalapKerulet():
 	a=input("Kérem a téglalap egyik oldalát[cm]:")	
 	b=input("Kérem a téglalap másik oldalát[cm]:")
 	return float(2*(a+b))
-	
+
+def teglalapTerulet():
+	a=input("Kérem a téglalap egyik oldalát[cm]:")	
+	b=input("Kérem a téglalap másik oldalát[cm]:")
+	return float(a*b)

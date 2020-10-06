4f17dc33007046000cca319e8d462127504e23d4
Szoftver Szilard <szoftteszt2020@gmail.com>
2020-10-06 19:08:51 +0200

    8szog

--- kerter.py
+++ kerter.py
@@ -8,3 +8,7 @@ def teglalapTerulet():
 	a=float(input("Kérem a téglalap egyik oldalát[cm]:"))	
 	b=float(input("Kérem a téglalap másik oldalát[cm]:"))
 	return float(a*b)
+
+def nyolcszogKerulet():
+	a=float(input("Kérem a 8szög oldalát[cm]:"))	
+	return float(8*a)

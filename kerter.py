b217e6ee430508182a91b1671ab50aab5a9800f1
Szoftver Szilard <szoftteszt2020@gmail.com>
2020-10-06 18:56:21 +0200

    Pontositas, mértékegység

--- kerter.py
+++ kerter.py
@@ -1,6 +1,6 @@
 # file létrehozva
 def teglalapKerulet():
-	a=input("Kérem a téglalap egyik oldalát:")	
-	b=input("Kérem a téglalap másik oldalát:")
+	a=input("Kérem a téglalap egyik oldalát[cm]:")	
+	b=input("Kérem a téglalap másik oldalát[cm]:")
 	return float(2*(a+b))
 	

63fe336bdbb2190bb36614b8d89a8860e9afc5ee
Ellenbacher Luca <ellenbacher.luca@gmail.com>
2020-10-06 19:04:39 +0200

    haromszog

--- kerter.py
+++ kerter.py
@@ -8,3 +8,9 @@ def teglalapTerulet():
 	a=float(input("Kérem a téglalap egyik oldalát[cm]:"))	
 	b=float(input("Kérem a téglalap másik oldalát[cm]:"))
 	return float(a*b)
+	
+def haromszogKerulet():
+	a=float(input("Kérem adja meg a háromszög egyik oldalát:")
+	b=float(input("Kérem adja meg a háromszög másik oldalát:")
+	c=float(input("Kérem adja meg a háromszög harmadik oldalát:")
+	return float()

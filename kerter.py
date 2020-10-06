a154ca0a0b2a4d5eee74f5be708d77afbea6d81d
Szoftver Szilard <szoftteszt2020@gmail.com>
2020-10-06 19:19:28 +0200

    Fő Program elkezdve, Háromszög javítva

--- kerter.py
+++ kerter.py
@@ -12,13 +12,33 @@ def teglalapTerulet():
 def nyolcszogKerulet():
 	a=float(input("Kérem a 8szög oldalát[cm]:"))	
 	return float(8*a)
+	
 def nyolcszogKerulet():
 	a=float(input("Kérem a 8szög oldalát[cm]:"))	
 	r=float(input("Kérem a 8szög sugarát[cm]:"))	
 	return float(4*a*r)
 	
 def haromszogKerulet():
-	a=float(input("Kérem adja meg a háromszög egyik oldalát:")
-	b=float(input("Kérem adja meg a háromszög másik oldalát:")
-	c=float(input("Kérem adja meg a háromszög harmadik oldalát:")
+	a=float(input("Kérem adja meg a háromszög egyik oldalát:"))
+	b=float(input("Kérem adja meg a háromszög másik oldalát:"))
+	c=float(input("Kérem adja meg a háromszög harmadik oldalát:"))
 	return float(a+b+c)
+	
+	
+print("1 - Háromszög")
+print("2 - Kör")
+print("3 - Teglalap")
+print("4 - Nyolcszög")
+v=input("Milyen alakzattal szeretnél dolgozni?")
+if v=="3":
+	print(teglalapKerulet())
+	print(teglalapTerulet())
+
+
+
+
+
+
+
+
+

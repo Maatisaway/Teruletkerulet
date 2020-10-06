9733b02455b86506a6525030e1699d77b303fb70
Szoftver Szilard <szoftteszt2020@gmail.com>
2020-10-06 19:00:18 +0200

    Teszt után javítva!

--- kerter.py
+++ kerter.py
@@ -1,10 +1,10 @@
 # file létrehozva
 def teglalapKerulet():
-	a=input("Kérem a téglalap egyik oldalát[cm]:")	
-	b=input("Kérem a téglalap másik oldalát[cm]:")
+	a=float(input("Kérem a téglalap egyik oldalát[cm]:"))	
+	b=float(input("Kérem a téglalap másik oldalát[cm]:"))
 	return float(2*(a+b))
 
 def teglalapTerulet():
-	a=input("Kérem a téglalap egyik oldalát[cm]:")	
-	b=input("Kérem a téglalap másik oldalát[cm]:")
+	a=float(input("Kérem a téglalap egyik oldalát[cm]:"))	
+	b=float(input("Kérem a téglalap másik oldalát[cm]:"))
 	return float(a*b)
